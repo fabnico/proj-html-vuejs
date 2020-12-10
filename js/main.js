cdd426ebe545c719af71e0d706ea65ef1731e31e
Fabrizio <whoet.wo@gmail.com>
2020-12-10 22:01:19 +0100

    Progetto completato

--- js/main.js
+++ js/main.js
@@ -8,6 +8,28 @@ const app = new Vue({
          'Features',
          'Blog',
          'Shop'
+      ],
+      footerExplore: [
+         'Start Here',
+         'Blog',
+         'About us'
+      ],
+      footerExplore2: [
+         'Success story',
+         'Courses',
+         'Contact us'
+      ],
+      footerInformation: [
+         'Membership',
+         'Purchase guide',
+         'Privacy Policy',
+         'Terms of service'
+      ],
+      footerSocial:[
+         'fab fa-facebook',
+         'fab fa-twitter',
+         'fab fa-instagram',
+         'fab fa-linkedin'
       ]
    }
 })
